01bfa0cccbd52df3feca3bacdfb4f809cc73da89
Afonso Pimenta <pimenta.afonsosilva@gmail.com>
2022-02-23 14:57:41 +0000

    update: refacture server code

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server open on port ${PORT}`);
 });
