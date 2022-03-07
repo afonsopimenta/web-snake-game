6e1ab4813c01554e689328cf4eb93accf00b4baf
Afonso Pimenta <pimenta.afonsosilva@gmail.com>
2022-03-07 15:14:18 +0000

    start serverside layout

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import http from "http";
+import {Server} from "socket.io";
+
+import createGame from "./scripts/game.js";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", socket => {
+  console.log(`> User connected: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`> User disconnected: ${socket.id}`);
+  });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`> Server listening on port: ${port}`);
+});
